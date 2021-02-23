bb361c6ea856051408246194ca33ea923f229149
bendro47 <57457556+bendro47@users.noreply.github.com>
2021-02-23 12:46:36 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int choice = 0;
+            int n;
+            while (choice == 0)
+            {
+                Console.WriteLine("Ask a Question:");
+                Console.ReadLine();
+                Random r = new Random();
+                n = r.Next(5);
+               
+                switch (n)
+                {
+                    case 0:
+                        Console.WriteLine("Yes");
+                        break;
+                    case 1:
+                        Console.WriteLine("No");
+                        break;
+                    case 2:
+                        Console.WriteLine("Possibly");
+                        break;
+                    case 3:
+                        Console.WriteLine("Quite Likely");
+                        break;
+                    case 4:
+                        Console.WriteLine("Try Again");
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("Enter 0 to go again Or 1 Quit");
+               choice=Convert.ToInt32(Console.ReadLine());
+
+            }
+
+        }
+ 
+    }
+
+}
